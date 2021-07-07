7aafed115a52e5f52967f29596edc9450d670a55
yzyouzhang <yzyouzhang@gmail.com>
2021-07-06 23:06:41 -0400

    add DF_aug and Melspec option

--- dataset.py
+++ dataset.py
@@ -101,11 +101,12 @@ class ASVspoof2019(Dataset):
 class ASVspoof2021LA_aug(Dataset):
     def __init__(self, path_to_ori="/data2/neil/ASVspoof2019LA/", path_to_augFeatures="/dataNVME/neil/ASVspoof2019LA_augFeatures", part="train", feature='LFCC', feat_len=750, pad_chop=True, padding='repeat'):
         super(ASVspoof2021LA_aug, self).__init__()
+        if feature == "Melspec":
+            path_to_augFeatures = "/data3/neil/ASVspoof2019LA_augFeatures"
         self.path_to_features = path_to_augFeatures
         self.part = part
         self.ori = os.path.join(path_to_ori, part)
         self.ptf = os.path.join(path_to_augFeatures, part)
-
         self.feat_len = feat_len
         self.feature = feature
         self.pad_chop = pad_chop
@@ -217,6 +218,70 @@ class ASVspoof2021LAeval(Dataset):
         if self.pad_chop:
             return default_collate(samples)
 
+
+class ASVspoof2021DF_aug(Dataset):
+    def __init__(self, path_to_ori="/data2/neil/ASVspoof2019LA/", path_to_augFeatures="/dataNVME/neil/ASVspoof2019DF_augFeatures", part="train", feature='LFCC', feat_len=750, pad_chop=True, padding='repeat'):
+        super(ASVspoof2021DF_aug, self).__init__()
+        if feature == "Melspec":
+            path_to_augFeatures = "/data3/neil/ASVspoof2019DF_augFeatures"
+        self.path_to_features = path_to_augFeatures
+        self.part = part
+        self.ori = os.path.join(path_to_ori, part)
+        self.ptf = os.path.join(path_to_augFeatures, part)
+        self.feat_len = feat_len
+        self.feature = feature
+        self.pad_chop = pad_chop
+        self.padding = padding
+        self.ori_files = librosa.util.find_files(os.path.join(self.ori, self.feature), ext="pt")
+        self.all_files = librosa.util.find_files(os.path.join(self.ptf, self.feature), ext="pt")
+        self.tag = {"-": 0, "A01": 1, "A02": 2, "A03": 3, "A04": 4, "A05": 5, "A06": 6}
+        self.label = {"spoof": 1, "bonafide": 0}
+        self.channel = ['no_channel', 'aac[16k]', 'aac[32k]', 'aac[8k]', 'mp3[16k]', 'mp3[32k]', 'mp3[8k]']
+        self.channel_dict = dict(zip(iter(self.channel), range(len(self.channel))))
+    def __len__(self):
+        return len(self.ori_files) + len(self.all_files)
+
+    def __getitem__(self, idx):
+        if idx < len(self.ori_files):
+            filepath = self.ori_files[idx]
+            basename = os.path.basename(filepath)
+            all_info = basename.split(".")[0].split("_")
+            assert len(all_info) == 6
+            channel = "no_channel"
+        else:
+            filepath = self.all_files[idx - len(self.ori_files)]
+            basename = os.path.basename(filepath)
+            all_info = basename.split(".")[0].split("_")
+            assert len(all_info) == 7
+            channel = all_info[6]
+        featureTensor = torch.load(filepath)
+        this_feat_len = featureTensor.shape[1]
+        if self.pad_chop:
+            if this_feat_len > self.feat_len:
+                startp = np.random.randint(this_feat_len - self.feat_len)
+                featureTensor = featureTensor[:, startp:startp + self.feat_len, :]
+            if this_feat_len < self.feat_len:
+                if self.padding == 'zero':
+                    featureTensor = padding_Tensor(featureTensor, self.feat_len)
+                elif self.padding == 'repeat':
+                    featureTensor = repeat_padding_Tensor(featureTensor, self.feat_len)
+                elif self.padding == 'silence':
+                    featureTensor = silence_padding_Tensor(featureTensor, self.feat_len)
+                else:
+                    raise ValueError('Padding should be zero or repeat!')
+        else:
+            pass
+        filename =  "_".join(all_info[1:4])
+        tag = self.tag[all_info[4]]
+        label = self.label[all_info[5]]
+        # return featureTensor, filename, tag, label, self.channel_dict[channel]
+        return featureTensor, filename, tag, label, channel
+
+    def collate_fn(self, samples):
+        if self.pad_chop:
+            return default_collate(samples)
+
+
 class ASVspoof2021DFeval(Dataset):
     def __init__(self, path_to_features="/dataNVME/neil/ASVspoof2021DFFeatures", feature='LFCC', feat_len=750, pad_chop=True, padding='repeat'):
         super(ASVspoof2021DFeval, self).__init__()
@@ -300,17 +365,17 @@ if __name__ == "__main__":
     # print(tag)
     # print(label)
 
-    training_set = ASVspoof2021LA_aug()
-    for i in range(25379, 26950):
-        feat_mat, filename, tag, label, channel = training_set[i]
+    # training_set = ASVspoof2021LA_aug()
+    # for i in range(25379, 26950):
+    #     feat_mat, filename, tag, label, channel = training_set[i]
     # print(len(training_set))
     # print(filename)
     # print(feat_mat.shape)
     # print(tag)
     # print(label)
     # print(channel)
-    print(len(training_set.channel))
-    print(training_set.channel)
+    # print(len(training_set.channel))
+    # print(training_set.channel)
 
 
     # trainDataLoader = DataLoader(training_set, batch_size=32, shuffle=True, num_workers=0, collate_fn=training_set.collate_fn)
@@ -328,4 +393,10 @@ if __name__ == "__main__":
     # print(tag)
     # print(label)
 
-    # libritts = LIBRITTS(root="/data/neil", download=True)
+    channel_lst = []
+    training_set = ASVspoof2021DF_aug()
+    for i in range(25379, 26950):
+        feat_mat, filename, tag, label, channel = training_set[i]
+        if not channel in channel_lst:
+            channel_lst.append(channel)
+    print(channel_lst)
